e78242dce4be6cc53f02873a0b59ed1c08921c85
Rizki Wahyu Pratama <69743247+putunebandi@users.noreply.github.com>
2026-04-01 23:44:58 +0700

    feat: menambahkan program komparasi dua buah string

---
+++ PerbandinganString.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val string1 = "Rizki Wahyu"
+    val string2 = "Rizki wahyu"
+    
+    if (string1 == string2) {
+        println("Kedua string sama")
+    } else {
+        println("Kedua string berbeda")
+    }
+}
