662e0688381b0c60681da4914282f927e40850a6
Rizki Wahyu Pratama <69743247+putunebandi@users.noreply.github.com>
2026-04-01 23:44:03 +0700

    feat: menambahkan program evaluasi batas angka 50 dan cek kelipatan 5

---
+++ KlasifikasiAngka.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val angka = 25 
+
+    if (angka > 50) {
+        println("Lebih dari 50")
+    } else if (angka < 50) {
+        println("Kurang dari 50")
+    }
+
+    if (angka % 5 == 0) {
+        println("Kelipatan 5")
+    } else {
+        println("Bukan kelipatan 5")
+    }
+}
